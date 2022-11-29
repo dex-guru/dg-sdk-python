518589726c2aff34bfc62f8bb2b6971df7606b6d
Evgeny Shestakov <evgeny.shestakov@techusage.com>
2022-11-29 18:43:28 +0100

    add native token field in chain model

--- dexguru_sdk/models/chain_models.py
+++ dexguru_sdk/models/chain_models.py
@@ -2,11 +2,14 @@ from typing import List
 
 from pydantic import BaseModel
 
+from dexguru_sdk.models.token_models import TokenInventoryModel
+
 
 class ChainModel(BaseModel):
     chain_id: int
     name: str
     description: str
+    native_token: TokenInventoryModel
 
 
 class ChainsListModel(BaseModel):
